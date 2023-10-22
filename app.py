c885eda47e99dc76185778132062b6602e324a28
ravikumarpal906 <ravikumarpal906@gmail.com>
2023-10-22 11:39:30 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template,request
+import requests
+
+app=Flask(__name__)
+
+@app.route("/")
+def hompage():
+    return render_template("index.html")
+
+@app.route("/weatherapp",methods=['POST','GET'])
+def get_weatherdata():
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q':request.form.get("city"),
+        'appid':request.form.get("appid"),
+        'units':'units'
+        }
+    respose = requests.get(url,params=params)
+    data = respose.json()
+    return f"data:{data}"
+    
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0")
